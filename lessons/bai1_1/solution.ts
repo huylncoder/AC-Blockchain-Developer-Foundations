9e3706364a5ca59e8e665f6c86924366bc6bd355
Le Nhut Huy <lenhuthuy8942@gmail.com>
2025-10-16 17:18:23 +0700

    Complete lesson 2.1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -11,7 +11,7 @@ export type Block = {
 // hàm kiểm tra tính hợp lệ của block
 export function isValidBlock(block: Block): boolean {
     // nối các biến dữ liệu thành chuỗi input cho hash
-    const dataToHash = `${block.index}${block.timestamp}${JSON.stringify(block.transactions)}${block.previous_hash}`;
+    const dataToHash = `${block.index}|${block.timestamp}|${JSON.stringify(block.transactions)}|${block.previous_hash}`;
     // tính hash SHA-256 của chuỗi dataToHash
     const computedHash = crypto.createHash('sha256').update(dataToHash).digest('hex');
     // so sánh hash tính được với current_hash của block
--- lessons/bai2_1/solution.ts
+++ lessons/bai2_1/solution.ts
@@ -3,13 +3,16 @@ export class SmartContract {
 
     constructor(initialMessage: string) {
         // TODO: initialize the message with initialMessage
+        this.message = initialMessage;
     }
 
     public updateMessage(newMsg: string): void {
         // TODO: update the message with newMsg
+        this.message = newMsg;
     }
 
     public getMessage(): string {
         // TODO: return the current message
+        return this.message;
     }
 }
