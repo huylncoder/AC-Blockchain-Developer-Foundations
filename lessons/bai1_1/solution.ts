7c66f03b8c01d7ec96dc80a2c499a12f731afb56
Le Nhut Huy <lenhuthuy8942@gmail.com>
2025-10-16 16:02:23 +0700

    Complete lesson 1.1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,13 +1,20 @@
+import * as crypto from "crypto";
 
 export type Block = {
-  index: number;
-  timestamp: string;
-  transactions: any[];
-  previous_hash: string;
-  current_hash: string;
+    index: number;
+    timestamp: string;
+    transactions: any[];
+    previous_hash: string;
+    current_hash: string;
 };
 
-// ✍️ TODO: Viết hàm tại đây
+// hàm kiểm tra tính hợp lệ của block
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+    // nối các biến dữ liệu thành chuỗi input cho hash
+    const dataToHash = `${block.index}${block.timestamp}${JSON.stringify(block.transactions)}${block.previous_hash}`;
+    // tính hash SHA-256 của chuỗi dataToHash
+    const computedHash = crypto.createHash('sha256').update(dataToHash).digest('hex');
+    // so sánh hash tính được với current_hash của block
+    return computedHash === block.current_hash;
 }
+
